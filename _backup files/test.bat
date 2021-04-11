620dfef948db35c52f74899bff5a02f175ef7797
Eddie Hartman <eddiehartman@Eddies-MBP.domain_not_set.invalid>
2021-04-11 11:23:28 +0200

    first

---
+++ _backup files/test.bat
@@ -0,0 +1,4 @@
+option confirm off
+open sftp://itrmop@us.ibm.com:oVrfvkuxPPzpNdJxxRx8bwx@tpywdcueng201.sl.bluecloud.ibm.com -hostkey="ssh-ed25519 255 MCsEYPeQdYT+Vqo9RitG9or7+XGOOaHuhA65YcId6PQ="
+put C:\SDISollutionDirectorySecond\POC_MVP\risk_20200727_AG.csv /dsaclproject/SARM-ECM/openpages/
+exit
